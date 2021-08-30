f4953f3827b356b2f194c2633b474c9e8bb09b38
Arun Sasi <38099892+getarun4t@users.noreply.github.com>
2021-08-30 16:51:22 +0200

    Added worker files (#4)

--- steps/drivers.py
+++ steps/drivers.py
@@ -4,6 +4,10 @@ from selenium import webdriver
 class Chromedriver:
     '''This can be used for saving driver settings'''
 
-    driver = webdriver.Remote(
-        command_executor='http://selenium:4444/wd/hub',
-        desired_capabilities={'browserName': 'chrome', 'javascriptEnabled': True})
+    chromedriver = 'chromedriver'
+    driver = webdriver.Chrome()
+
+    # driver = webdriver.Remote(
+    #     command_executor='http://selenium:4444/wd/hub',
+    #     desired_capabilities={'browserName': 'chrome', 'javascriptEnabled': True})
+    driver.maximize_window()
--- steps/test_001_campaign.py
+++ steps/test_001_campaign.py
@@ -1,6 +1,35 @@
 from pytest_bdd import scenario, when, then, parsers
-from steps.Actions.common_actions import PageActions
+from steps.common_actions import PageActions
+
+actions = PageActions()
+
 
 @scenario('../features/campaign page.feature', 'User is verifying the list of safety features')
 def test_safety():
     pass
+
+@scenario('../features/campaign page.feature', 'User is verifying the list of testimonials')
+def test_testimonials():
+    pass
+
+@scenario('../features/campaign page.feature', 'User is verifying the Innovation')
+def test_innovation():
+    pass
+
+@scenario('../features/campaign page.feature', 'User is verifying the list of Models')
+def test_models():
+    pass
+
+@scenario('../features/campaign page.feature', 'User is verifying the Top Panel')
+def test_top_panel():
+    pass
+
+
+@when(parsers.parse('user clicks on {button} in {page}'))
+def click_action(button, page):
+    actions.click_button(button, page)
+
+
+@then(parsers.parse('the below options should be available in the {page}:\n{options}'))
+def list_check(page, options):
+    actions.check_list(page, options)
